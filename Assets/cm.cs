2787f6ab62cd887d871fe39c9b0d1401fa5c614b
Vesab <168362412+vesab0@users.noreply.github.com>
2024-08-18 19:37:36 +0200

    trying to fix cells going into each other

---
+++ Assets/cm.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cm : MonoBehaviour
+{
+    public bool TouchingWall;
+    public GameObject katrori;
+    private bool hasInstantiatedPrefab;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "platform" || other.gameObject.tag== "cantMove")
+        {
+            TouchingWall = true;
+            if (!hasInstantiatedPrefab)
+            {
+                Instantiate(katrori, transform.position, Quaternion.identity);
+                hasInstantiatedPrefab = true; // Mark it as instantiated
+            }
+            if(other.gameObject.tag =="cantMove"){
+                Debug.Log("e preka cantmovin");
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "platform"|| other.gameObject.tag== "cantMove")
+        {
+            TouchingWall = false;
+
+        }
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    public float cellSize = 1f;
+    public cd cd;
+    public cm cm;
+    public bool canmovetag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+
+        
+        
+        if(cm.TouchingWall == false){
+                
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position = transform.position + new Vector3(-1,0,0);
+            }
+        }
+        if(cd.TouchingWall == false){
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position = transform.position + new Vector3(1,0,0);
+            }
+        }  
+
+         
+    }
+}
